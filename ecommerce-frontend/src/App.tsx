e721027752a479dd326140c0ec68901874c14a45
NADUNSANDEEPA <48161752+NADUNSANDEEPA@users.noreply.github.com>
2025-08-09 06:58:58 +0000

    frontend init

--- ecommerce-frontend/src/App.tsx
+++ ecommerce-frontend/src/App.tsx
@@ -2,14 +2,18 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 
-function App() {
+const App: React.FC = () => {
   return (
     <Router>
+      <nav style={{ padding: "10px", background: "#eee" }}>
+        <Link to="/" style={{ marginRight: "10px" }}>Home</Link>
+      </nav>
+
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
-}
+};
 
 export default App;

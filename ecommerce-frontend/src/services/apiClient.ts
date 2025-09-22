065c43718c991467dc011bf704dada91c65ef283
NADUNSANDEEPA <nadun2952@gmail.com>
2025-09-22 06:41:38 +0530

    update : api gateway

---
+++ ecommerce-frontend/src/services/apiClient.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+  baseURL: process.env.REACT_APP_API_BASE_URL, 
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+
+apiClient.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("authToken"); 
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default apiClient;
